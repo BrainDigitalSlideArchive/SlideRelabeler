a613161af7f18189b599fb243de592d8a5178aa7
Aaron Rosado <arosad2@protonmail.ch>
2025-06-19 12:56:18 -0400

    Update help.

--- src/containers/Modal/ModalHelp.jsx
+++ src/containers/Modal/ModalHelp.jsx
@@ -14,11 +14,11 @@ function ModalHelp(props) {
           <h2>Application</h2>
           <p>
             The SlideRelabeler application is designed to simplify the process of deidentifying whole slide images so
-          that they can be shared for research purposes.  The purposes of deidentification is protect patients from the
-            misuse of their personal information.  The SlideRelabeler application is designed to remove information from
+          that they can be shared for research purposes.  Deidentification protects patients from the
+            misuse of their personal information.  The SlideRelabeler application is designed to remove personal health information (PHI) from
             whole slide images that could be used to identify a patient.  Although the application is designed to do so
             you should make sure that the application is working as expected before sharing the images given differences
-            can exist between how universities and hospitals conduct whole slide imaging and how this applications work
+            can exist between how universities and hospitals conduct whole slide imaging and how this application works
             with those whole slide images.  Given this application's <b>developers accept no liability in a failure of this application
             to remove sensitive patient information from whole slide images, the user of this application is liable for its'
             inappropriate use and the failure to check if an output file has been completely deidentified.</b>  We encourage users
@@ -34,10 +34,10 @@ function ModalHelp(props) {
             files to the application. The third workflow is to use a csv file containing
             the information about where a file is stored on your computer/organization's network and then process those files.
             In all cases the application runs the same process to remove patient identifying information from the files.
-            After processing the files your work will be exported to a csv file that you can use to track the files you deidentified
-            if you need to refer back to them in the future. This information can allow linking back to the original information that
-            could potentially be used to identify a patient so you should store it in a secure location.  If you share this csv file
-            with an outside entity/institution you should make sure sensitive health information is removed from that version of
+            As the program processes files, a csv file is generated that tracks deidentified progress and can help refer back to 
+            original file if needed. This information can allow linking back to the original information that
+            could potentially be used to identify a patient so you should store it in an encrypted, secure, HIPPA compliant location.  
+            If you share this csv file with an outside entity/institution you should make sure PHI is removed from that version of
             the file.
           </p>
           <p>
@@ -54,13 +54,16 @@ function ModalHelp(props) {
             To use the application with a folder of whole slide images you can use the "Add folder" button to select
             a directory of input files. To select multiple files you can hold down the control button or use shift to select
             a starting and ending folder within a sequence. You can repeat this process as many time as needed to add all the
-            files you wish to process.  You should see all your added files in the table within the main window.  Once you are finished
+            files you wish to process.  Adding folders will recursively add all files from selected folders and subfolders.
+            You should see all your added files in the table within the main window.  Once you are finished
             adding files you can select an output folder with the "Choose output dir" button.  After having the appropriate setup for
             processing you should be able to use the "process files" button to begin the deidentification procedure.
           </p>
           <p>
-            To use the application with a csv file you must make a CSV file with a data column labeled "filename" that has a filename in the directory of the
-            selected csv file or a relative/absolute path to the whole slide image file.  Additional metadata columns from this
+            To use the application with a csv file you must make a CSV file with a data column labeled "path" that has a filename in the directory of the
+            selected csv file or a relative/absolute path to the whole slide image file.  If you don't have a column labeled "path" you can 
+            select a column from your csv file that contains the paths for your files.  Please read more below for additional information 
+            about the csv feature to learn how to specify a rename and metadata column.  Additional metadata columns from this
             file will be added to your table and you can use this table to inspect the files loaded using your csv. Once the files are
              loaded you must select an output directory using the "Choose output dir" button in the main window.  If your setup is
             ready for processing you will see the "process files" button become enabled which you can press to begin
@@ -79,8 +82,8 @@ function ModalHelp(props) {
           <p>
             Filename configuration is by default a client side generated uuid based on the input file's path.
             If you select the rename option the output filename will by default match the input filename unless you've
-            loaded files using a csv file featuring a "rename" column.  If you use the rename feature you can change the
-            rename value in the main window's table.  You are able to add text by default to all output filenames either
+            loaded files using a csv file featuring a rename column and setup your CSV input accordingly.  If you use the rename 
+            feature you can change the rename value in the main window's table.  You are able to add text by default to all output filenames either
             at the beginning ("prefix") or ending ("suffix").  You can alter the possible fields here to see an example of
             what the output filename would look like.
           </p>
@@ -92,13 +95,27 @@ function ModalHelp(props) {
             Label configuration by default will add a top section of text corresponding to the renamed filename and a QR code with
             that same output filename.  You can add an icon here by selecting an image file on your computer that will be added
             to the label between any text or QR code that you've specified to be placed into your output label.  You can disable the
-            label's text, icon, and QR code by toggling the appropriate checkboxes.  The text in the label can be changes to any of
+            label's text, icon, and QR code by toggling the appropriate checkboxes.  The text in the label can be changed to any of
             the table's current columns.  The QR mode can be changed to either contain just the file's uuid, json from selected
             table column data (base64 encoded utf-8 json), or text from a specific table column's data.
           </p>
           <p>
-            CSV configuration by default creates an output CSV file in your output directory with a deid_output.csv filename.
+            CSV configuration allows you to control relevant settings for CSV input and output.  You can specify in the text 
+            a column that contains the filename for your target files.  Using this field is required by the CSV import functionality
+            if you do not put in the correct column name, you will be given an opportunity to select it before processing the files. 
+            There are also optional columns you can specify that control the filename rename feature or where the file gets outputed.
+            By default the application creates an output CSV file in your output directory with a deid_output.csv filename.
             You can toggle whether to save the output csv file or not using the labeled checkbox in this section.
+          </p>
+        </div>
+        <div className={"__content-section"}>
+          <h2>Previewing De-identification Output</h2>
+          <p>
+            The applicaiton allows for previewing the de-identification output before processing the files.  
+            This can be done by clicking the file's thumbnail and opening the viewer window. The viewer window
+            will allow you to visualize the file and inspect the current associated images 
+            (thumbnail, label, and macro image if available).  You can aslo see the preview of the replacement label 
+            and macro image.
           </p>
         </div>
         <div className={"__content-section"}>
@@ -107,17 +124,19 @@ function ModalHelp(props) {
           <p>Click "Add Folder" to add a folder or multiple folder to the files table.</p>
           <p>Click "CSV Import" to add files from rows in a CSV file to the files table.</p>
           <p>Click "Choose Output Dir" to select a directory to copy the processed files into.</p>
+          <p>Click "Choose CSV File" to select a CSV file to import files from.</p>
           <p>Click "Clear Files" to remove all files from the files table.</p>
           <p>Click on the question icon to open up the currently viewed help menu.</p>
           <p>Click on the cogwheel icon to open up configuration options for the output.</p>
           <p>Click "Process Files" to copy files from the input directory to the output directory</p>
+          <p>Click the "Cancel" button to stop the de-identification process.  This can be resumed by clicking the "Process Files" button again.</p>
         </div>
         <div className={"__content-section"}>
           <h2>How to Contribute</h2>
           <p>
             This application is a work in progress and is dependent upon feedback you provide to improve the application
             and ensure it works as expected.  If you have any feedback please provide it to the developers of the application
-            <a href={"mailto:arosad2@protonmail.ch"}>Aaron Rosado</a> and <a href={"mailto:"}>Tom Pierce</a>
+            <a href={"mailto:arosad2@protonmail.ch"}>Aaron Rosado</a> and <a href={"mailto:pearcetm@upmc.edu"}>Tom Pierce</a>
           </p>
         </div>
       </div>
